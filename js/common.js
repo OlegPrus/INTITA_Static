9233eadebc6894d742d3982b281dcd0e83ef492d
OlegPrus <oleg.prus.first@gmail.com>
2016-06-18 11:32:41 +0300

    Работает карусель, БургерМеню

---
+++ js/common.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+	$("#owl-slider").owlCarousel({
+		// Most important owl features
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+      items : 1,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+	});
+
+});
